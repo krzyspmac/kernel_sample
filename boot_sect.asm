8fe7b56b1f92c0a241d9488e4b1406aba88756ab
Krzysztof Pawłowski <krzyspmac@gmail.com>
2023-10-24 22:25:49 +0200

    Cleaner boot sector app

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,32 +1,20 @@
 ;
-;   A simple boot sector program that starts and loops
+; Boot sector program that prints a starting sting
 ;
 
-;
-; Print a message to screen
-;
+[org 0x7c00]                        ; Tell where this code starts in memory
 
-    mov ah, 0x0e                    ; int 10/ah = 0eh -> scrolling teletype
+    mov si, HELLO_MSG               ; Use SI as function parameter
+    call print_string
 
-    mov al, 'H'
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, '!'
-    int 0x10
+    jmp $
 
-loop:                               ; Label for loop, used to allow infite
-    jmp loop                         ; looping; the idea is so that we never
-                                    ; end the program once started
+%include "print_string.asm"
 
-    times 510-($-$$) db 0           ; Add padding to 512 bytes
+; Data
+HELLO_MSG:
+    db `Hello, World!`, 0           ; null-terminated string
 
-dw 0xaa55                           ; Last two bytes (1 word) is the magic
-                                    ; number that informs BIOS that is is
-                                    ; indeed the boot sector
+; Padding and magic number
+    times 510-($-$$) db 0
+    dw 0xaa55
